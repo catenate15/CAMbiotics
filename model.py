96527231d74ae44890dc8f8e07d956aa29a0cd36
Abdulmujeeb Toluwase Onawole <14907281+catenate15@users.noreply.github.com>
2024-01-22 11:50:24 +1000

    Update model.py
    Updated model architecure and make number of layers and units flexible to change.

--- model.py
+++ model.py
@@ -1,127 +1,134 @@
+
+
 import torch
+import logging
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
+from torchmetrics import MetricCollection
+from torchmetrics.classification import BinaryAccuracy, BinaryF1Score, BinaryCohenKappa, BinaryMatthewsCorrCoef, BinarySpecificity, BinaryAUROC
+import json
 
-from torchmetrics.classification import (
-    BinaryAccuracy,
-    F1Score as BinaryF1Score,
-    CohenKappa as BinaryCohenKappa,
-    MatthewsCorrcoef as BinaryMatthewsCorrCoef,
-    Specificity as BinarySpecificity,
-    AUROC as BinaryAUROC
-)
+logger = logging.getLogger(__name__)
+# Set up basic configuration for logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-class SMILESClassifier(pl.LightningModule):
-    def __init__(
-        self, 
-        vocab_size, # The number of unique characters in the SMILES alphabet
-        embedding_dim=128, # The size of the embedding vectors
-        cnn_filters=64, # The number of filters in the convolutional layers
-        lstm_units=64, # The number of units in the LSTM layer
-        output_size=1,# The number of units in the output layer
-        learning_rate=1e-3 # The learning rate for the optimizer,  
-    ):
-        super(SMILESClassifier, self).__init__()
-        
-        # Embedding layer that turns tokens into embeddings
-        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim)
+class ConvLSTMCAMbiotic(pl.LightningModule):
+    """
+    ConvLSTMCAMbiotic is a PyTorch Lightning Module that contains the model architecture and the training loops.
+    """
+    def __init__(self, base_filename, num_cnn_layers=5, num_lstm_layers=2, hidden_dim=64, output_size=1, learning_rate=1e-3):
+        super().__init__()
+        self.save_hyperparameters()
+
+        # Load unique characters
+        unique_chars_file = f"{base_filename}_unique_chars.json"
+        with open(unique_chars_file, 'r') as file:
+            unique_chars = json.load(file)
+
+        # Number of unique characters
+        num_unique_chars = len(unique_chars)
 
         # Convolutional layers
-        self.conv1 = nn.Conv1d(in_channels=embedding_dim, out_channels=cnn_filters, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv1d(in_channels=cnn_filters, out_channels=cnn_filters, kernel_size=3, padding=1)
+        self.convs = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv1d(in_channels=num_unique_chars if i == 0 else hidden_dim,
+                          out_channels=hidden_dim, kernel_size=3, padding='same'),
+                nn.ReLU(),
+                nn.MaxPool1d(kernel_size=2),
+                nn.Dropout(0.3),
+                nn.BatchNorm1d(num_features=hidden_dim)
+            ) for i in range(num_cnn_layers)
+        ])
+
+        # LSTM layers
+        self.lstm = nn.LSTM(input_size=hidden_dim, hidden_size=hidden_dim,
+                            batch_first=True, num_layers=num_lstm_layers)
+
+        # Fully connected layer
+        self.fc = nn.Linear(in_features=hidden_dim, out_features=output_size)
         
-        # LSTM layer
-        self.lstm = nn.LSTM(input_size=cnn_filters, hidden_size=lstm_units, batch_first=True)
+        
+        # Metric collection
+        metrics = MetricCollection({
+            'accuracy': BinaryAccuracy(),
+            'f1_score': BinaryF1Score(),
+            'cohen_kappa': BinaryCohenKappa(),
+            'mcc': BinaryMatthewsCorrCoef(),
+            'specificity': BinarySpecificity(),
+            'auroc':  BinaryAUROC()
+        })
+
+        # Metric collection for training
+        self.train_metrics = metrics.clone(prefix='train_')
+        self.val_metrics = metrics.clone(prefix='val_')
+        self.test_metrics = metrics.clone(prefix='test_')
+
+    @staticmethod
+    def save_hyperparameters_to_json(hparams, filepath):
+        # Check if hparams is an instance of AttributeDict or a similar object
+        # If yes, convert it to a dictionary
+        if not isinstance(hparams, dict):
+            hparams = vars(hparams)
+
+        # Save the dictionary to a JSON file
+        with open(filepath, 'w') as f:
+            json.dump(hparams, f, indent=4)
+
+
+    
+    def forward(self, x):
+        # Permute the tensor to have the shape [batch_size, num_channels, seq_len]
+        x = x.permute(0, 2, 1)
+
+        # Apply each convolutional layer
+        for conv in self.convs:
+            x = conv(x)
+
+        # LSTM layer(s)
+        x, _ = self.lstm(x)
+        x = x[:, -1, :]  # Use only the last output of the LSTM
         
         # Fully connected layer
-        self.fc = nn.Linear(in_features=lstm_units, out_features=output_size)
+        x = self.fc(x)
+        x = torch.sigmoid(x).squeeze()  # Sigmoid activation function used since it is binary classification
+        return x
 
-         # Hyperparameters
-        self.learning_rate = learning_rate
 
 
-        # Metrics
-        self.accuracy = BinaryAccuracy()
-        self.f1_score = BinaryF1Score()
-        self.cohen_kappa = BinaryCohenKappa()
-        self.mcc = BinaryMatthewsCorrCoef()
-        self.specificity = BinarySpecificity()
-        self.auroc = BinaryAUROC()
-        
-    def forward(self, x, return_cam=False): # set return_cam to True if you want to return the feature maps if not set to false during model training
-        # Pass the input through the embedding layer
-        print(f"Input shape: {x.shape}")
-        x = self.embedding(x)
-        print(f"After embedding shape: {x.shape}")
-        # Convolutional layers expect input in the shape (batch_size, channels, sequence_length)
-        # so we permute the output from the embedding layer
-        
-        x = x.permute(0, 2, 1)
-        print(f"After permute shape: {x.shape}")
-        # Pass through convolutional layers with activation function and pooling
-        x1 = F.relu(self.conv1(x)) #x1: The output of the first convolutional layer
-        x2 = F.relu(self.conv2(x1)) #x2: The output of the second convolutional layer
-        
-        # LSTM layer
-        x, _ = self.lstm(x2) #x: The output of the LSTM layer
-        
-        # We're using the output of the last time step for the fully connected layer
-        x = self.fc(x[:, -1, :]) #x: The output of the fully connected layer
-        
-        # Apply a sigmoid activation function for binary classification
-        x = torch.sigmoid(x) #x: The final output of the model
-        
-        if return_cam:
-            # If return_cam is True, return both the prediction and the feature maps
-            return x, x2
-        else:
-            return x
+
 
     def configure_optimizers(self):
-        # Using the learning rate hyperparameter
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+    def compute_metrics(self, y_hat, y, metrics):
+        y_hat_sigmoid = torch.sigmoid(y_hat)
+        return metrics(y_hat_sigmoid, y.int())
+    
+    def step(self, batch, batch_idx, metrics):
+        x, y = batch
+        x, y = x.to(self.device), y.to(self.device)
+        y_hat = self(x)
+        # Print the shape of y_hat for the first 5 batches
+        if batch_idx < 5:
+            print(f"Batch {batch_idx} - y_hat shape before squeeze:", y_hat.shape)
+
+
+        # If y_hat has more than 1 dimension and the second dimension is 1, squeeze it.
+        if y_hat.dim() > 1 and y_hat.shape[1] == 1:
+            y_hat = y_hat.squeeze(1)
+
+        loss = F.binary_cross_entropy_with_logits(y_hat, y)
+        self.log_dict(self.compute_metrics(y_hat, y, metrics), on_step=True, on_epoch=True)
+        self.log(f'{metrics.prefix}loss', loss, on_step=True, on_epoch=True)
+        return loss
 
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.binary_cross_entropy(y_hat, y)
-        self.log('train_loss', loss)
-        # Log metrics
-        self.log('train_accuracy', self.accuracy(y_hat, y.int()))
-        self.log('train_f1_score', self.f1_score(y_hat, y.int()))
-        self.log('train_cohen_kappa', self.cohen_kappa(y_hat, y.int()))
-        self.log('train_mcc', self.mcc(y_hat, y.int()))
-        self.log('train_specificity', self.specificity(y_hat, y.int()))
-        self.log('train_auroc', self.auroc(y_hat, y))
-        return loss
+        return self.step(batch, batch_idx, self.train_metrics)
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.binary_cross_entropy(y_hat, y)
-        self.log('val_loss', loss)
-        # Log metrics
-        self.log('val_accuracy', self.accuracy(y_hat, y.int()))
-        self.log('val_f1_score', self.f1_score(y_hat, y.int()))
-        self.log('val_cohen_kappa', self.cohen_kappa(y_hat, y.int()))
-        self.log('val_mcc', self.mcc(y_hat, y.int()))
-        self.log('val_specificity', self.specificity(y_hat, y.int()))
-        self.log('val_auroc', self.auroc(y_hat, y))
-        return
+        return self.step(batch, batch_idx, self.val_metrics)
 
     def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.binary_cross_entropy(y_hat, y)
-        self.log('test_loss', loss)
-        # Log metrics
-        self.log('test_accuracy', self.accuracy(y_hat, y.int()))
-        self.log('test_f1_score', self.f1_score(y_hat, y.int()))
-        self.log('test_cohen_kappa', self.cohen_kappa(y_hat, y.int()))
-        self.log('test_mcc', self.mcc(y_hat, y.int()))
-        self.log('test_specificity', self.specificity(y_hat, y.int()))
-        self.log('test_auroc', self.auroc(y_hat, y))
-        return loss
+        return self.step(batch, batch_idx, self.test_metrics)
